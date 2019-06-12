b4c60f91c48a732f17cabd725ec2e6f285cb41e4
Thales <thekheysermanu@gmail.com>
2019-06-12 16:01:43 +0200

    add routes and webpack config for js heap out of memory pb

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import React, { SyntheticEvent, Component } from 'react';
-import FirstComponent from './firstComponent';
+import {FirstComponent} from './firstComponent';
 import './app.scss';
 import { createStyles, withStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 import { Theme } from '@material-ui/core';
+import * as Constants from './constants';
+import { Link } from 'react-router-dom';
 
 const styles = (theme: Theme) => createStyles({
   root: {
@@ -41,6 +43,11 @@ class AppComponent extends React.Component<IAppComponentProps> {
         <Typography className={classes.typo} component='h3' gutterBottom={true}>
           Hello, Welcome to my first Snake game page from {this.props.framework}
         </Typography>
+        <ul>
+          <li>
+            <Link to={Constants.STATS_URL}>Stats</Link>
+          </li>
+        </ul>
         <FirstComponent />
       </div>
     );
